1d8b963d89f25ea0e68a83d770ec12137b7a935e
Liber142 <dimaliber142@gmail.com>
2025-06-06 00:37:44 +1000

    Change parsing to JSON

--- include/Parsing.h
+++ include/Parsing.h
@@ -4,8 +4,8 @@
 class Parsing
 {
 private:
-	static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp);
-	static std::string fetch_html(const std::string& url);
+	static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+	static std::string fetchData(const std::string& url);
 	static std::string UrlCode(const std::string &value);
 public:
 	static std::string GetUrl(std::string name);
--- src/Parsing.cpp
+++ src/Parsing.cpp
@@ -1,79 +1,76 @@
 #include <curl/curl.h>
+#include <nlohmann/json.hpp>
+#include <ostream>
 #include <string>
 #include <sstream>
 #include <cstddef>
 
 #include "../include/Parsing.h"
 
-size_t Parsing::WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp) 
+using json = nlohmann::json;
+
+size_t Parsing::WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) 
 {
-    userp->append((char*)contents, size * nmemb);
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
-std::string Parsing::fetch_html(const std::string& url) 
+std::string Parsing::fetchData(const std::string& url) 
 {
-    CURL* curl = curl_easy_init();
-    std::string response;
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
 
-    if (curl) {
+    curl = curl_easy_init();
+    if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // Разрешаем редиректы
-        
-        // Устанавливаем User-Agent
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0");
-
-        CURLcode res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            std::cerr << "cURL error: " << curl_easy_strerror(res) << std::endl;
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         }
-        
         curl_easy_cleanup(curl);
     }
-    
-    return response;
+    return readBuffer;
 }
 
-std::string Parsing::UrlCode(const std::string &value)
-{
-	
-	std::string encoded;
-	for (char c : value) 
-	{
-	    std::stringstream ss;
-	    ss << "-" << static_cast<int>(static_cast<unsigned char>(c)) << "-";
-	    encoded += ss.str();
-	}
-	return encoded;
+std::string Parsing::UrlCode(const std::string &value) {
+    std::ostringstream encoded;
+    encoded.fill('0');
+    encoded << std::hex; // Устанавливаем шестнадцатеричный формат
+
+    for (char c : value) {
+        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+            encoded << c; // Оставляем символ как есть, если он допустим в URL
+        } else {
+            encoded << '%' << std::setw(2) << static_cast<int>(static_cast<unsigned char>(c));
+        }
+    }
+
+    return encoded.str();
 }
 
 std::string Parsing::GetUrl(std::string name)
 {
-	std::string url = "https://ddnet.org/players/" + UrlCode(name) + "/";
+	std::string url = "https://ddnet.org/players/?json2=" + UrlCode(name) ;
+	std::cout << "url: " << url << std::endl;
 	return url;
 }
 
 
-
 int Parsing::GetPoints(std::string url)
 {
-	std::string html = fetch_html(url);
-	if (!html.empty())
-	{
-		if (html.find("<title>Player not found") != std::string::npos)
-		{
-			std::cout << "Player not found" << std::endl;
-			return -1;
-		}
-		else
-		{
-			int startPos = html.find("with", html.find("<h3>Points (")) + 5;
-			int length = html.find(" points", startPos) - startPos;
-			int points = std::stoi(html.substr(startPos, length)) ;
-			return points;
-		}
-	}
-	return 0;
+    std::string strData = fetchData(url);
+    //std::cout << "Response data: " << strData << std::endl; // Debugging output
+
+    json jsonData = json::parse(strData);
+
+    if (jsonData.contains("points") && jsonData["points"].contains("points")) {
+        auto points = jsonData["points"]["points"];
+        return points.get<int>();
+    } else {
+        std::cerr << "The JSON data does not contain the 'points' key." << std::endl;
+        return 0; // or handle the error appropriately
+    }
 }
