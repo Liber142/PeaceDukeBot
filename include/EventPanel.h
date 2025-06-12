0486ca881eb0ce9c4fdbbbd6efa4c71aaa82c04a
Liber142 <dimaliber142@gmail.com>
2025-06-12 11:20:20 +1000

    Add event panel

--- include/EventPanel.h
+++ include/EventPanel.h
@@ -9,9 +9,11 @@ private:
 	dpp::snowflake eventRole_id;
 	dpp::snowflake channel_id;
 	std::string eventEmorion;
-public:
-	EventPanel(dpp::cluster& bot);
+
 	void SetEventRole(dpp::snowflake& role_id);
 	void SetEventChannel(dpp::snowflake& channel_id);
 	void SetEventEmotion(std::string& emotionid);
+public:
+	EventPanel(dpp::cluster& bot);
+	void ButtonHandler(const dpp::button_click_t& event);
 };
