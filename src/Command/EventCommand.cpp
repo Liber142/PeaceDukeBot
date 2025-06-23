58345d27eb9d0d6b29efc53abc47a745f74c382d
Liber142 <dimaliber142@gmail.com>
2025-06-23 16:51:57 +1000

    Add command for start events

---
+++ src/Command/EventCommand.cpp
@@ -0,0 +1,30 @@
+#include "../../include/Commands/EventCommand.h"
+#include <dpp/appcommand.h>
+#include <dpp/dispatcher.h>
+
+EventCommand::EventCommand(dpp::cluster& bot_instance) : bot(bot_instance) {};
+
+void EventCommand::Execute(const dpp::slashcommand_t& event)
+{
+    if (/*event.command.get_issuing_user().is_certified_moderator()*/true)
+    {
+        dpp::message msg(event.command.channel_id, "\t\tЗдесь вы можете организовать свой ивент. \nПосле нажатия кнопки ниже вам будет необходимо заполнить параметры ивент\n```\t- Роль которая будет использоваться для организации ивента\n\t- Время в которое проведёться ивент\n\t- Роль которая сможет принять участие в ивенте\n\t- Наполнение сообщения для ивента```\nВаш ивент будет опубликован в канале обьявлений после принятия модерацией\nТакже у вас будет возможность пинговать участников ивента");
+                
+                msg.add_component(
+                    dpp::component().add_component(
+                        dpp::component()
+                            .set_label("Начать ивент")
+                            .set_type(dpp::cot_button)
+                            .set_style(dpp::cos_primary)
+                            .set_id("start_event")
+                    )
+                );
+            
+        event.reply(msg);    
+    }
+}
+
+dpp::slashcommand EventCommand::Register()
+{
+    return  dpp::slashcommand("apply", "Создаст сообщение с кнопкой для подачи заявки", bot.me.id);
+}
