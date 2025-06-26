1b1ab2e1874b9af6418b3158127a86cea38a2d3e
Liber142 <dimaliber142@gmail.com>
2025-06-26 14:26:52 +1000

    Fix

--- src/Online.cpp
+++ src/Online.cpp
@@ -64,7 +64,8 @@ void C_OnlineClanMember::ParsData(nlohmann::json data)
 		{
         	if (!data["addresses"].empty()) 
         	{
-            	newServer.ip = data["addresses"][0];
+        		std::cout << "addresses content: " << data["addresses"].dump(2) << std::endl;
+            	newServer.ip = data["addresses"][0].get<std::string>();
 				std::cout << "5" << std::endl;
 				newServer.connectUrl = "https://ddnet.org/connect-to/?addr=" + newServer.ip.substr(13);
             }
