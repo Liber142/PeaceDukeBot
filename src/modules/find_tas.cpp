050c0c104c531bf153c9e82335a61ed013842b9d
Liber142 <dimaliber142@gmail.com>
2026-09-25 14:06:51 +1000

    refactor: Find tass class
    Now class use ddnet api "deleted_rank"

--- src/modules/find_tas.cpp
+++ src/modules/find_tas.cpp
@@ -2,9 +2,13 @@
 
 #include <execution>
 #include <mutex>
+#include <iomanip>
+#include <sstream>
 
 #include <engine/logger.h>
 #include <engine/console.h>
+#include <engine/database.h>
+#include <engine/data_strucs.h>
 
 #include <cpr/cpr.h>
 
@@ -17,6 +21,7 @@ void CFindTas::OnConsoleInit()
 	Console()->Register("find_player", {"s"}, 0, [this](const CConsole::IResult &Result) { FindPlayer(Result.GetString(0)); }, "Find player by name");
 	Console()->Register("find_map", {"s"}, 0, [this](const CConsole::IResult &Result) { FindMap(Result.GetString(0)); }, "Find map by name");
 	Console()->Register("check_player", {"s"}, 0, [this](const CConsole::IResult &Result) { CheckPlayer(Result.GetString(0)); }, "Check TAS ranks on player by nickname");
+	Console()->Register("check_clan", {""}, 0, [this](const CConsole::IResult &Result) { CheckClan(); }, "Check TAS ranks on player by nickname");
 }
 
 void CFindTas::CheckPlayer(const std::string &Nick)
@@ -26,6 +31,7 @@ void CFindTas::CheckPlayer(const std::string &Nick)
 		Player = GetPlayer(Nick);
 	} catch (...) {
 		CLogger::Error(Name(), "Player not found");
+		return;
 	}
 
 	std::vector<CFinish> TasRanks;
@@ -41,15 +47,44 @@ void CFindTas::CheckPlayer(const std::string &Nick)
 			}
 		});
 
+	if(TasRanks.size() <= 0)
+		return;
+
+	CLogger::Info(Name(), Player.m_Nick);
+
 	CLogger::Info(Name(), "Num TAS ranks: " + std::to_string(TasRanks.size()));
 	for(const auto &Finish : TasRanks)
 	{
 		std::stringstream Str;
 		Str	<< std::left
-			<< std::setw(3) << Finish.m_Rank
-		    << std::setw(24) << Finish.m_Name 
-			<< std::setw(10) << Finish.m_Server;
+		    << std::setw(24) << Finish.m_Name;
 		CLogger::Info(Name(), Str.str());
+	}
+	CLogger::Info(Name(), "-----");
+}
+
+void CFindTas::CheckClan()
+{
+	auto Keys = DataBase()->GetKeys("clan_members");
+	std::mutex Mutex;
+	std::vector<std::string> Nicks;
+
+	for(const auto& Key : Keys)
+	{
+		auto User = DataBase()->Load<SUserData>("clan_members", Key);
+		if(User)
+			Nicks.emplace_back(User->m_GameNick);
+	}
+
+	std::for_each(std::execution::par,
+		Nicks.begin(), Nicks.end(),
+		[&](const auto &Nick) {
+			std::lock_guard<std::mutex> Lock(Mutex);
+			CheckPlayer(Nick);
+	});
+	
+	for(auto& Key : Keys)
+	{
 	}
 }
 
@@ -58,58 +93,23 @@ bool CFindTas::IsTas(const CFinish &Finish)
 	if(Finish.m_Server == "Fun")
 		return false;
 
-	CMap Map;
-	try 
-	{
-		Map = GetMap(Finish.m_Name);
-	}
-	catch(...)
-	{
-		CLogger::Error(Name(), "Map not found");
-		return false;
-	}
-
-
-	const int &TargetRank = Finish.m_Rank;
-	std::vector<IRank*> TasRanks;
-	int CountRanks = 0;
-	if(Finish.m_IsTeamRank)
-	{
-		for(const auto &Rank : Map.m_TeamRanks)
-		{
-			if(Rank.m_Rank == TargetRank)
-				CountRanks++;
-		}
-	}
-	else 
-	{
-		for(const auto &Rank : Map.m_Ranks)
-		{
-			if(Rank.m_Rank == TargetRank)
-				CountRanks++;
-		}
-	}
-	 
-	return CountRanks > 1;
+	return true;
 }
+
 
 void CFindTas::PrintPlayer(const CFindTas::CPlayer &Player) const
 {
 	CLogger::Info(Name(), Player.m_Nick);
 	std::stringstream Str;
 	Str << std::left 
-		<< std::setw(24) << "Map"
-		<< std::setw(10) << "Server"
-		<< std::setw(5) << "Rank";
+		<< std::setw(24) << "Map";
 	CLogger::Info(Name(), Str.str());
 	Str.str("");
 
 	for(const auto &Finish : Player.m_Finishes)
 	{
 		Str << std::left 
-			<< std::setw(24) << Finish.m_Name 
-			<< std::setw(10) << Finish.m_Server 
-			<< std::setw(5) << Finish.m_Rank;
+			<< std::setw(24) << Finish.m_Name;
 
 		CLogger::Info(Name(), Str.str());
 		Str.str("");
@@ -118,49 +118,12 @@ void CFindTas::PrintPlayer(const CFindTas::CPlayer &Player) const
 
 void CFindTas::PrintMap(const CFindTas::CMap &Map) const
 {
-	std::stringstream Str;
-	Str << std::left 
-		<< std::setw(5) << "Rank"
-		<< std::setw(24) << "Nick";
-	CLogger::Info(Name(), Str.str());
-	Str.str("");
-
-	for(const auto &Rank : Map.m_TeamRanks)
-	{
-		Str << std::left 
-			<< std::setw(5) << Rank.m_Rank
-			<< std::setw(24) << Rank.m_Nicks[0];
-
-		CLogger::Info(Name(), Str.str());
-		Str.str("");
-
-		if(Rank.m_Rank > 10)
-			break;
-	}
+	CLogger::Info(Name(), "Map printing is no longer supported by this API backend.");
 }
 
 CFindTas::CMap CFindTas::GetMap(const std::string &MapName)
 {
-	if(m_CacheMap.contains(MapName))
-		return m_CacheMap[MapName];
-
-	cpr::Response r = cpr::Get(cpr::Url{"https://ddstats.tw/map/json"},
-					  cpr::Parameters{{"map", MapName}});
-
-	if(r.status_code / 100 != 2)
-		throw;
-
-	nlohmann::json j;
-
-	try { j = nlohmann::json::parse(r.text); } 
-	catch (const std::exception &e) { 
-		CLogger::Error(Name(), e.what());	
-		throw;
-	}
-
-	CMap Map = from_json(j, Map);
-	m_CacheMap[MapName] = Map;
-	return Map;
+	return CMap{};
 }
 
 CFindTas::CPlayer CFindTas::GetPlayer(const std::string &Nick)
@@ -168,8 +131,9 @@ CFindTas::CPlayer CFindTas::GetPlayer(const std::string &Nick)
 	if(m_CachePlayer.contains(Nick))
 		return m_CachePlayer[Nick];
 
-	cpr::Response r = cpr::Get(cpr::Url{"https://ddstats.tw/player/json"},
-					  cpr::Parameters{{"player", Nick}});
+	cpr::Response r = cpr::Get(cpr::Url{"https://ddnet.org/players/"},
+					  cpr::Parameters{{"json2", Nick}});
+
 
 	if(r.status_code / 100 != 2)
 		throw;
@@ -183,21 +147,14 @@ CFindTas::CPlayer CFindTas::GetPlayer(const std::string &Nick)
 	}
 
 	CPlayer Player = from_json(j, Player);
+	Player.m_Nick = Nick;
 	m_CachePlayer[Nick] = Player;
 	return Player;
 }
 
 void CFindTas::FindMap(const std::string &MapName)
 {
-	try 
-	{
-		CMap Map = GetMap(MapName);
-		PrintMap(Map);
-	}
-	catch(...) 
-	{
-		CLogger::Warning(Name(), "Map not found");
-	};
+	CLogger::Warning(Name(), "Global map search is no longer available.");
 }
 
 void CFindTas::FindPlayer(const std::string &Nick)
@@ -216,165 +173,42 @@ void CFindTas::FindPlayer(const std::string &Nick)
 CFindTas::CPlayer CFindTas::from_json(const nlohmann::json &j, const CPlayer &Type) const
 {
 	CPlayer Result;
-	try
+	if(j.contains("deleted_ranks"))
 	{
-		if(j.contains("profile") && j["profile"].contains("name"))
-			Result.m_Nick = j["profile"].value("name", "");	
-		else
-		 	CLogger::Warning(Name(), "Missing player name");
-	}
-	catch(const std::exception &e)
-	{
-		CLogger::Error(Name() + " profile name", e.what());
-	}
-
-	if(j.contains("finishes"))
-	{
-		for(const auto &RawFinish : j["finishes"])
+		for(const auto &RawFinish : j["deleted_ranks"])
 		{
 			CFinish Finish = from_json(RawFinish, Finish);
-			if(Finish.m_Rank > 0 && Finish.m_Rank <= 100)
-				Result.m_Finishes.emplace_back(std::move(Finish));
+			Result.m_Finishes.emplace_back(std::move(Finish));
 		}	
-
-		auto &Finishes = Result.m_Finishes;
-		std::sort(Finishes.begin(), Finishes.end(),
-			[](const CFinish &a, const CFinish &b) {
-				return a.m_Rank < b.m_Rank;
-			});
 	}
 	else 
-		CLogger::Warning(Name(), "Missing finishes");
+		CLogger::Warning(Name(), "Missing deleted_ranks");
 	return Result;
 }
 
-CFindTas::CMap 
-CFindTas::from_json(const nlohmann::json &j, const CMap &Type) const
-{
-	CMap Result;
-	try 
-	{
-		if(j.contains("rankings"))
-		{
-			for(const auto &JsonRank : j["rankings"])
-			{
-				CRank Rank = from_json(JsonRank, Rank);
-				Result.m_Ranks.push_back(std::move(Rank));	
-			}
-		}
-		if(j.contains("team_rankings"))
-		{
-			for(const auto &JsonRank : j["team_rankings"])
-			{
-				CTeamRank TeamRank = from_json(JsonRank, TeamRank);
-				Result.m_TeamRanks.push_back(std::move(TeamRank));	
-			}
-		}
-	}
-	catch(const std::exception &e)
-	{
-		CLogger::Error(Name(), e.what());
-	}
-	return Result;
-}
-
-CFindTas::CRank 
-CFindTas::from_json(const nlohmann::json &j, const CRank &Type) const
-{
-	CRank Result;
-	try
-	{
-		if(j.contains("rank"))
-			Result.m_Rank = j.value("rank", 0);
-		else 
-			CLogger::Warning(Name(), "Missing rank");
-
-		if(j.contains("name"))
-			Result.m_Nick = j.value("name", "");
-		else 
-			CLogger::Warning(Name(), "Missing name");
-	}
-	catch(const std::exception &e)
-	{
-		CLogger::Error(Name(), e.what());
-	}
-	return Result;
-}
-
-CFindTas::CTeamRank 
-CFindTas::from_json(const nlohmann::json &j, const CTeamRank &Type) const
-{
-	CTeamRank Result;
-	try
-	{
-		if(j.contains("rank"))
-			Result.m_Rank = j.value("rank", 0);
-		else 
-			CLogger::Warning(Name(), "Missing rank");
-
-		if(j.contains("players"))
-		{
-			for(const auto &Player : j["players"])
-				Result.m_Nicks.emplace_back(Player);
-		}
-		else 
-			CLogger::Warning(Name(), "Missing players");
-	}
-	catch(const std::exception &e)
-	{
-		CLogger::Error(Name(), e.what());
-	}
-	return Result;
-}
-
-CFindTas::CFinish 
-CFindTas::from_json(const nlohmann::json &j, const CFinish &Type) const
+CFindTas::CFinish CFindTas::from_json(const nlohmann::json &j, const CFinish &Type) const
 {
 	CFinish Result;
+	Result.m_Rank = 0; 
+	Result.m_IsTeamRank = false;
+
 	try 
 	{
 		if(j.contains("map"))
-		{
-			if(j["map"].contains("map"))
-				Result.m_Name = j["map"].value("map", "hui");
-			else 
-				CLogger::Warning(Name(), "Missing map name");
-			if(j.contains("server"))
-				Result.m_Server = j["map"].value("server", "hui");
-			else 
-				CLogger::Warning(Name(), "Missing map type");
-		}
-	}
-	catch(const std::exception &e)
-	{
-		CLogger::Error(Name() + "_map", e.what());
-	}
+			Result.m_Name = j.value("map", "hui");
+		else 
+			CLogger::Warning(Name(), "Missing map name");
 
-	try 
-	{
-		try 
-        {
-			if(j.contains("team_rank"))
-			{
-				Result.m_Rank = j.value("team_rank", 0);
-				Result.m_IsTeamRank = true;
-			}
-		}	
-		catch(const std::exception &e)
-		{
-			if(j.contains("rank"))
-			{
-				Result.m_Rank = j.value("rank", 0);
-				Result.m_IsTeamRank = false;
-			}
-			else 
-				CLogger::Warning(Name(), "Missing rank");
-		}
+		if(j.contains("country"))
+			Result.m_Server = j.value("country", "hui");
+		else 
+			CLogger::Warning(Name(), "Missing country");
 	}
 	catch(const std::exception &e)
 	{
-		CLogger::Error(Name() + "_rank", e.what());
+		CLogger::Error(Name(), e.what());
 	}
 
 	return Result;
 }
+
--- src/modules/find_tas.h
+++ src/modules/find_tas.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "module.h"
 #include <dpp/json.h>
+#include <unordered_map>
+#include <vector>
+#include <string>
 
 class CFindTas : public IModule
 {
@@ -13,39 +16,21 @@ public:
 		int m_Rank;
 	};
 
-	struct IRank 
-	{
-		int m_Rank;
-	};
-
-	struct CRank : IRank
-	{
-		std::string m_Nick;
-	};
-
-	struct CTeamRank : IRank
-	{
-		std::vector<std::string> m_Nicks;
-	};
-
-	struct CMap 
-	{
-		std::vector<CTeamRank> m_TeamRanks;
-		std::vector<CRank> m_Ranks;
-	};
-
 	struct CPlayer 
 	{
 		std::string m_Nick;
 		std::vector<CFinish> m_Finishes;
 	};
 
+	struct CMap {};
+
 	void OnInit() override;
 	void OnConsoleInit() override;
 
 	void PrintPlayer(const CPlayer &Player) const;
-	void PrintMap(const CFindTas::CMap &Map) const;
+	void PrintMap(const CMap &Map) const;
 	void CheckPlayer(const std::string &Nick);
+	void CheckClan();
 	bool IsTas(const CFinish &Finish);
 
 	const std::string Name() const override { return "find_tas"; }
@@ -59,11 +44,7 @@ private:
 
 	CFinish from_json(const nlohmann::json &j, const CFinish &Type) const;
 	CPlayer from_json(const nlohmann::json &j, const CPlayer &Type) const;
-	CMap from_json(const nlohmann::json &j, const CMap &Type) const;
-	CRank from_json(const nlohmann::json &j, const CRank &Type) const;
-	CTeamRank from_json(const nlohmann::json &j, const CTeamRank &Type) const;
 
-	std::unordered_map<std::string, CMap> m_CacheMap;
 	std::unordered_map<std::string, CPlayer> m_CachePlayer;
-
 };
+
